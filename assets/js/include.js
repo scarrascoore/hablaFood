ee157488e50db1c2f8c75a6ffbc694fe77456290
scarrasco <scarrascoore@gmail.com>
2025-05-08 11:02:24 -0500

    Creacion de include.js

---
+++ assets/js/include.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("components/header.html")
+      .then(res => res.text())
+      .then(data => document.getElementById("header-placeholder").innerHTML = data);
+  
+    fetch("components/footer.html")
+      .then(res => res.text())
+      .then(data => document.getElementById("footer-placeholder").innerHTML = data);
+  });
+  
+  
+  
