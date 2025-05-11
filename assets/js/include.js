840a4d835f014f31bd2cc2962b0ffb52a2e146bc
scarrasco <scarrascoore@gmail.com>
2025-05-11 03:05:03 -0500

    Primeras configuraciones de Geolocalizacion

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("components/header.html")
+    fetch("../components/header.html")
       .then(res => res.text())
       .then(data => document.getElementById("header-placeholder").innerHTML = data);
   
-    fetch("components/footer.html")
+    fetch("../components/footer.html")
       .then(res => res.text())
       .then(data => document.getElementById("footer-placeholder").innerHTML = data);
   });
